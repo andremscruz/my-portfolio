3490e228072aa99f3375f638d02cd188f95cabdf
Andre Cruz <71467368+andremscruz@users.noreply.github.com>
2025-10-02 01:44:43 +0100

    style changes

--- src/components/OtherCard.js
+++ src/components/OtherCard.js
@@ -8,11 +8,11 @@ function OtherCard(props){
             <h3 className="other-cards-title">{props.other.title}</h3>
             <div className="other-cards-image-div">
                 {props.other.link && 
-                    <a href={props.other.link} target="blank" style={{backgroundColor: '#1e1e1e'}}>
-                        <img src={`/images/${props.other.image}`} alt="" className="other-cards-logo"></img>
+                    <a href={props.other.link} target="blank" style={{backgroundColor: '#1e1e1e'}} className="other-cards-logo">
+                        <img src={`/images/${props.other.image}`} alt="" style={{backgroundColor: '#1e1e1e'}}></img>
                     </a> 
                 }
-                <a href={props.other.gitLink} target="blank" className="other-cards-git"> 
+                <a href={props.other.gitLink} target="blank" className="other-cards-git" style={props.other.link ? null : {marginTop:'4px', fontSize:'unset'}}> 
                     <FaGithub style={props.other.link ? {backgroundColor: '#1e1e1e', marginTop: '25px'} : {backgroundColor: '#1e1e1e'}}/> 
                 </a>
             </div>
