92b565a7fe4e639bde941876e3eb73dedca97a82
Andre Cruz <71467368+andremscruz@users.noreply.github.com>
2025-10-14 17:32:18 +0100

    preparing files to expand projects cards

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -3,20 +3,39 @@ import {mainProjects} from "../data"
 import { otherProjects } from "../data";
 import MainCard from "./MainCard";
 import OtherCard from "./OtherCard";
+import Description from "./Description";
 
 function Cards() {
-    const[toggle, setToggle] = useState(false)
+    const [toggle, setToggle] = useState(false)
+    const [showDescription, setShowDescription] = useState(false)
+    const [description, setDescription] = useState({
+        title: "",
+        description: "",
+        icon: "",
+        img: "",
+        live: "",
+        repo: ""
+    })
 
     function toggleFunction(){
         setToggle(prevState => !prevState)
     }
+    function showDesc(card){
+        setShowDescription(prevState => !prevState)
+        setDescription(card)
+    }
+
     return(
         <>
+            {!showDescription ?
+            <>
             <main className="main--cards">
                 <div className="cards-div">
-                    {mainProjects.map(main =>
-                        <MainCard key={main.title} main={main}/>
-                    )}
+                    {
+                      mainProjects.map(main =>
+                        <MainCard key={main.title} main={main} handleClick={showDesc}/>
+                        )   
+                    }
                 </div>
             </main>
             <div className="expand-projects-div" onClick={toggleFunction}>
@@ -33,6 +52,11 @@ function Cards() {
                     )}
                 </div>
             }
+            </>
+            :
+            <Description description={description} />
+            }
+            
         </>
     )
 }
